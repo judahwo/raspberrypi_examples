a259c9a4fdd543366b93d26afd1bec6c6cca2922
Marcus Worrell <mjw7d@mtmail.mtsu.edu>
2023-09-14 16:25:50 -0500

    Create echolocation.py

---
+++ echolocation.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+trigPin = 23
+echoPin = 24
+
+GPIO.setup(trigPin, GPIO.OUT)
+GPIO.setup(echoPin, GPIO.IN)
+
+try:
+    while True:
+        GPIO.output(trigPin, 0)
+        time.sleep(2E-6)
+        GPIO.output(trigPin, 1)
+        time.sleep(10E-6)
+        GPIO.output(trigPin, 0)
+        while GPIO.input(echoPin) == 0:
+            pass
+        echoStartTime = time.time()
+        while GPIO.input(echoPin) == 1:
+            pass
+        echoStopTime = time.time()
+        pingTravelTime = echoStopTime - echoStartTime
+        print(int(pingTravelTime*1E6))
+        time.sleep(.2)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print('GPIO Clean')
